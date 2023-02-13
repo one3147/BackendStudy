841c5c09503f047d2a715b8d1776ea4d426ed352
Dowonjune <cantonetab0306@gmail.com>
2023-02-13 12:04:34 +0900

    I am Backend assistant

---
+++ dowonjune 2/src/test/java/com/iStudyJava/dowonjune/DowonjuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iStudyJava.dowonjune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowonjuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
