a0f6c3da803b0fb22a1f9c10fbcef3c6f16eb12f
Dowonjune <cantonetab0306@gmail.com>
2023-02-17 20:56:37 +0900

    CRUD 시작...ㅠ-ㅠ

--- board/src/main/java/com/example/board/LoginController.java
+++ board/src/main/java/com/example/board/LoginController.java
@@ -1,30 +1,38 @@
 package com.example.board;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @PostMapping("/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model)
-    {
-        // 로그인 처리
-        boolean success = false;
-        if (username.equals("admin") && password.equals("admin")) {
-            success = true;
-        }
-
-        if (success) {
-            // 로그인 성공시 메인 페이지로 이동
+    public String loginCheck(@RequestParam String username, @RequestParam String password) {
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        try {
+            User user = jdbcTemplate.queryForObject(sql, new Object[] {username, password}, new BeanPropertyRowMapper<>(User.class));
             return "main";
-        } else {
-            // 로그인 실패시 에러 메시지 표시
-            model.addAttribute("error", "Invalid username or password");
-            return "login";
+        } catch (EmptyResultDataAccessException e) {
+            return "index";
         }
     }
+    @GetMapping("/perfect")
+    public String login(@RequestParam String name)
+    {
+        if (name.equals("admin"))
+        {
+            return "<h1>Success</h1>";
+        }
+        return "<h1>Failed..</h1>";
 
+    }
 }
